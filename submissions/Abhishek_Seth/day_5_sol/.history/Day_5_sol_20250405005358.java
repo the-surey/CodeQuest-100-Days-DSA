29057e6648195465ecc3fc92655f7e4684999f12
Abhishek05eccs <abhishek.seth_ec23@gla.ac.in>
2025-04-06 23:19:27 +0530

    day6_decision.java

---
+++ submissions/Abhishek_Seth/day_5_sol/.history/Day_5_sol_20250405005358.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Day_5_sol{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a=sc.nextInt();
+        int i;
+        for(int i = 1; i <= a; i++) {
+            
+        }
+    }
+}
