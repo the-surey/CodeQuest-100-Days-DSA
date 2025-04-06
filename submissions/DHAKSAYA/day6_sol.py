c3750b0ecbcf5a78be454d25829404bc0ad47b6a
Dhaksaya <mkda2006@gmail.com>
2025-04-06 18:47:15 +0530

    day6

---
+++ submissions/DHAKSAYA/day6_sol.py
@@ -0,0 +1,7 @@
+a=input("Which path do you choose?(left/right):")
+if(a== "left" or a== "Left"):
+    print("You found a hidden tunnel! You’re safe. 🚀")
+elif(a=="right" or a=="Right"):
+    print("Oh no! The Glitch’s trap was here! Try again. 😨")    
+else:
+    print("Invalid choice. Please enter 'left' or 'right'.")    
