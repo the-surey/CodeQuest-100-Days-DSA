da60574f910ce794f788e9551ddf74c0c8f98dcc
Mohan18Kumar <133189507+Mohan18Kumar@users.noreply.github.com>
2025-04-07 13:52:20 +0530

    day7 solutions (#102)
    * Mohan Kumar
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * day6
    
    * update day_6 solution
    
    * day1_sol
    
    * solution day6_decisions
    
    * Delete day6_decisions
    
    * create day7 solution

---
+++ submissions/MohanKumarS/day7/day7_loops.py
@@ -0,0 +1,3 @@
+word = input("Enter the secret word: ") # gets the word from the user that you want to print
+for i in range(5): # for loop -> I need to print it for 5 times -> range function goes from (0 to 5-1)
+    print(word) # prints the word you want to print
