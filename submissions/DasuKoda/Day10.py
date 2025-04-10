689be03a89501c678c1cf4652c590bbdbc5765bc
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-04-11 00:14:11 +0530

    day_10_sol (#165)

---
+++ submissions/DasuKoda/Day10.py
@@ -0,0 +1,22 @@
+import re
+def is_strong(password):
+    if len(password)<8:
+        return False
+    if not re.search(r'[A-Z]',password):
+        return False
+    if not re.search(r'[a-z]',password):
+        return False
+    if not re.search(r'[0-9]',password):
+        return False
+    if not re.search(r'[@#%^&*()!~:"<>?:"]',password):
+        return False
+    return True
+
+
+
+
+p=input("Enter Your password :")
+if(is_strong(p)):
+    print("Strong Password")
+else:
+    print("Weak Password (Missing uppercase letter and special character)")
