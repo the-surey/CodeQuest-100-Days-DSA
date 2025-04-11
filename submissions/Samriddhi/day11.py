f0bb6d3c8354acafb5f03a3366741cedca0d56dc
Avantikaay <raunakyadav0717@gmail.com>
2025-04-11 20:46:29 +0530

    Create day11.py

---
+++ submissions/Samriddhi/day11.py
@@ -0,0 +1,4 @@
+k=input("Enter values separated by space")
+k=k.split() #split will convert the string into a list
+m=sorted(k) #sort will sort the list in ascending order
+print(m) #print the sorted list
