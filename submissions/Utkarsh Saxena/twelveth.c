55e4e6384f0301c7192be6510468df44231de94a
Utkarsh-0003 <utkarsh.saxena_cs23@gla.ac.in>
2025-04-12 23:28:53 +0530

    my_day_12_solution

---
+++ submissions/Utkarsh Saxena/twelveth.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int N;
+    printf("Enter N "); //Taking input from user
+    scanf("%d", &N);
+     for(int i=0;i<N;i++){
+        printf("ACCESS GRANTED\n"); // printing the number of times wanted
+     }
+     return 0;
+}
