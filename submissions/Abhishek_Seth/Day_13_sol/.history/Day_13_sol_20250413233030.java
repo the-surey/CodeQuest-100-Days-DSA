d56196b896d555be5b9a21099005326bc628240f
Abhishek05eccs <abhishek.seth_ec23@gla.ac.in>
2025-04-13 23:33:22 +0530

    day13_coordinates.java

---
+++ submissions/Abhishek_Seth/Day_13_sol/.history/Day_13_sol_20250413233030.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Day_13_sol{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int n;
+        System.out.print("Enter N: ");
+        n=sc.nextInt();
+        int d,i=n,c=0;
+        while(i!=0){
+            d=i%10;
+            if(d%2==0){
+                c++;
+            }
+            i=i/10;
+        }
+        i=n;
+        if(c>0){
+            int arr[]=new int[c];
+            while(i!=0){
+                d=i%10;
+                if(d%2==0){
+                    arr[i]=d;
+                }
+                i=i/10;
+            }
+            System.out.print("Even digits: ");
+            for( i = c-1; i >= 0; i--) {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        else{
+            System.out.println("No even digits found!");
+        }
+    }
+}
