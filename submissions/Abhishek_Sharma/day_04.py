0d9a1f3c1532371c7af195a289e802493ad9ac8f
Abhishek Sharma <abhisheksharma182005@gmail.com>
2025-04-17 23:04:25 +0530

    Update day_04.py

--- submissions/Abhishek_Sharma/day_04.py
+++ submissions/Abhishek_Sharma/day_04.py
@@ -6,3 +6,15 @@ for i in number:
     sum += int(i)
 expression = " + " .join(digits)
 print(f"Sum of digits: {expression} = {sum}")
+
+number = input("Enter a number:- \n")
+sum = 0
+expression = ""
+
+for i in number:
+    sum += int(i)  # Calculating the sum of digits
+    if expression:  # If it's not the first digit, add " + " before the number
+        expression += " + "
+    expression += i  # Adding the current digit to the expression
+
+print(f"Sum of digits: {expression} = {sum}")
