08d4346cb877c6345512a87abb6cca23a3d26acc
Abhishek Sharma <abhisheksharma182005@gmail.com>
2025-04-18 17:22:44 +0530

    Create Day-08.py

---
+++ submissions/Abhishek_Sharma/Day-08.py
@@ -0,0 +1,6 @@
+for i in range(10, 0, -1):  # Need colon, step should be -1 to count down
+    if i == 1:               # Need colon, no parentheses needed
+        print("1")        # Print this when i is 1
+    else:
+        print(i)             # Print regular numbers otherwise
+print("Boom! Just kidding, you saved the city!")  # Final message after loop
