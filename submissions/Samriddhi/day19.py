40b330ac9720bbfab78475c2b5efbd21727b56ee
Avantikaay <raunakyadav0717@gmail.com>
2025-04-20 00:11:10 +0530

    Create day19.py (#326)

---
+++ submissions/Samriddhi/day19.py
@@ -0,0 +1,5 @@
+#program to take the list as input and sort it in ascending order
+k=input("Enter the list: ")#taking input from the user
+l=eval(k)#evaluating the input to convert it to a list
+p=l.sort()#sorting the list in ascending order
+print("The sorted list is:",l)#printing the sorted list
