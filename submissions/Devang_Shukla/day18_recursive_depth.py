9497cee36e44a9d40d954dcf3e8475f152f7ee0e
Devilwelcometohell <devangshukla218@gmail.com>
2025-04-19 07:37:45 -0700

    day18_recursive_depth.py

---
+++ submissions/Devang_Shukla/day18_recursive_depth.py
@@ -0,0 +1,11 @@
+def find_depth(structure):
+    if not isinstance(structure,list):
+        return 0
+    elif not structure:
+        return 1
+    else:
+        return 1 + max(find_depth(item) for item in structure)
+# Example input
+maze = [1,[2,[3,[4]]]] 
+depth = find_depth(maze)
+print(f"The maze has a depth of {depth}.")   
