5ad52ed0d2aaf1f5ee78ccdf0abb44bd5de9bfbc
Dasu Koda <22MH1A04H3@acoe.edu.in>
2025-04-21 19:42:09 +0530

    Day_21_sol
    This is my day21 solution

---
+++ submissions/DasuKoda/Day21.py
@@ -0,0 +1,21 @@
+a=list(input("Enter Numbers : "))
+
+for i in range(0,len(a)):
+    if i%2==0:
+        print(a[i],end=' ')
+
+
+
+
+
+"""
+
+Enter Numbers : 5871435647
+5 7 4 5 4 
+
+
+Enter Numbers : 123456789
+1 3 5 7 9 
+
+
+"""
