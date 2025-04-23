740678d6c0c35ffbab6f681c0a5799f5bddd056e
KRISHNA GUPTA <babakrishnadas16402@gmail.com>
2025-04-23 21:06:51 +0530

    Day5_sol_loops.java

---
+++ submissions/KRISHNA GUPTA/Day5_sol.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Day5_sol
+{
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner (System.in);
+        int n;
+        n=sc.nextInt();
+        for (int i=1; i<=n; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
