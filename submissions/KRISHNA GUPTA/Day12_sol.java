ef220f0bf449f10ade23d970a6202c5be2dd2de7
KRISHNA GUPTA <babakrishnadas16402@gmail.com>
2025-04-23 22:51:35 +0530

    Day12_sol_passcode.java

---
+++ submissions/KRISHNA GUPTA/Day12_sol.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Day12_sol {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter N: ");
+        int n;
+        n=sc.nextInt();
+        for(int i=1; i<=n; i++)
+        {
+            System.out.println("ACCESS GRANTED");
+        }
+    }
+    
+}
