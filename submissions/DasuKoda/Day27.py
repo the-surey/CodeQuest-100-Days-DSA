1c9e1d41a5309ae33043b4242b65a0f1a21189bd
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-04-28 00:11:51 +0530

    Day27 (#399)
    * Day_21_sol
    
    This is my day21 solution
    
    * day_22_sol
    
    This is my Day22 solution
    
    * Day23_sol
    
    This is my Day23 solution
    
    * Day24_sol
    
    This is my day 24 solution
    
    * Day_25_sol
    
    Thia is my Day 25 solution
    
    * Day_26_sol
    
    This is my Day 26 solution
    
    * Day_27_ sol
    
    This is my Day27 solution

---
+++ submissions/DasuKoda/Day27.py
@@ -0,0 +1,10 @@
+module=list(map(int,input("Enter the Input sequance : ").split()))
+print([m for m in module if m <50 ])
+
+
+
+
+"""
+Enter the Input sequance : 90 80 70 49 30 20 
+[49, 30, 20]
+"""
